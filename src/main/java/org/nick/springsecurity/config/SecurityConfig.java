0ffa3c64e54124913d966545f5987a26781a8243
DeliriumTraemens <oskinnick@gmail.com>
2021-09-29 14:16:41 +0300

    Initial commit

---
+++ src/main/java/org/nick/springsecurity/config/SecurityConfig.java
@@ -0,0 +1,86 @@
+package org.nick.springsecurity.config;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Bean;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.core.userdetails.User;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.provisioning.InMemoryUserDetailsManager;
+import org.springframework.security.provisioning.JdbcUserDetailsManager;
+
+import javax.sql.DataSource;
+
+@EnableWebSecurity
+public class SecurityConfig extends WebSecurityConfigurerAdapter {
+	@Autowired
+	DataSource dataSource;
+	
+	@Override
+	protected void configure(HttpSecurity http) throws Exception {
+		http.authorizeRequests()
+				.antMatchers("/authenticated/**").authenticated()/*Доступ в аутентифицированные адреса*/
+					.and()
+//				.httpBasic()
+				.formLogin()
+//				.loginPage("/login")
+				.permitAll()
+					.and()
+				.logout()
+				.permitAll();
+	}
+//	---------------------- JDBC AUTH ----------------------
+	@Bean
+	public JdbcUserDetailsManager users(DataSource dataSource){
+		
+		UserDetails user = User.builder()
+				.username("user")
+				.password("{bcrypt}$2a$12$6GDz.wzfTTXZwL26H8BGYOnG2HxxrBD7es2Qh9J9hJMAwxeHORboS")
+				.roles("USER")
+				.build();
+
+		UserDetails admin = User.builder()
+                .username("admin")
+                .password("{bcrypt}$2a$12$G.kaE47bsKMFrTQTQfWNW.MkYnvv.sDQAT0ABOTvXaw23s3fi5K.m")
+                .roles("USER","ADMIN")
+                .build();
+		
+		JdbcUserDetailsManager jdbcUserDetailsManager = new JdbcUserDetailsManager(dataSource);
+		
+		if (jdbcUserDetailsManager.userExists(user.getUsername())) {
+			jdbcUserDetailsManager.deleteUser(user.getUsername());
+		}
+		
+		if(jdbcUserDetailsManager.userExists(admin.getUsername())){
+			jdbcUserDetailsManager.deleteUser(admin.getUsername());
+		}
+		
+		jdbcUserDetailsManager.createUser(user);
+		jdbcUserDetailsManager.createUser(admin);
+		
+		return jdbcUserDetailsManager;
+	}
+	
+//	-------------------- IN MEMORY AUTH -------------------
+//	@Bean
+//	public UserDetailsService users(){
+//		UserDetails user = User.builder()
+//				.username("user")
+//				.password("{bcrypt}$2a$12$6GDz.wzfTTXZwL26H8BGYOnG2HxxrBD7es2Qh9J9hJMAwxeHORboS")
+//				.roles("USER")
+//				.build();
+//
+//		UserDetails admin = User.builder()
+//                .username("admin")
+//                .password("{bcrypt}$2a$12$G.kaE47bsKMFrTQTQfWNW.MkYnvv.sDQAT0ABOTvXaw23s3fi5K.m")
+//                .roles("USER","ADMIN")
+//                .build();
+//
+//		return new InMemoryUserDetailsManager(user, admin);
+//	}
+}
+
+//.antMatchers("/admin/**").hasAnyRole("ADMIN","SUPERADMIN")/*Допуск в админку по ролям*/
+//.antMatchers("/profile/**").authenticated()/*См выше - может быть и hasAuthority*/
---
+++ src/main/java/org/nick/springsecurity/controller/MainController.java
@@ -0,0 +1,39 @@
+package org.nick.springsecurity.controller;
+
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
+@RestController
+public class MainController {
+	
+	List <String> names = Arrays.asList("John", "Sarrah", "O'Connor");
+	
+	@GetMapping("/")
+	public String home(){
+		return "Home page";
+	}
+	
+	@GetMapping("/authenticated")
+	public String pageForAuthenticatedUsers(){
+		Authentication auth = SecurityContextHolder.getContext().getAuthentication();
+		
+		return "Secured part of Web Service\n "+auth;
+	}
+	
+	@GetMapping("/hello")
+	public String sayHello(){
+		return "HI Say Hello";
+	}
+	
+	@GetMapping("/names")
+	public List names(){
+		return names;
+	}
+	
+}
